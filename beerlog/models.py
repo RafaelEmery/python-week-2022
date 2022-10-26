715b25a83f148270c18f5c88556e65361cfb8668
RafaelEmery <rafael.emerycade@gmail.com>
2022-10-26 19:57:53 -0300

    using sqlmodel, sqlalchemy and pydantic on models file

--- beerlog/models.py
+++ beerlog/models.py
@@ -1,31 +1,46 @@
-# Importing a dataclass function of dataclasses module to start modeling data
-# The dataclass function allow us to define objects as dataclasses
-# It's an easy way to persist data on Python 3
-# More info: https://docs.python.org/3/library/dataclasses.html
-from dataclasses import dataclass
+# Using sqlmodel module and SQLModel model and Field function
+# SQLModel is a simple ORM . Docs: https://sqlmodel.tiangolo.com/
+from sqlmodel import SQLModel, Field
+# Using select function from ORM
+# from sqlmodel import select
+# Using typing module to set id as optional when creating an object
+from typing import Optional
+# Importing Pydantic library to use validator function
+from pydantic import validator 
 
-# Defining a "simple" Beer model
-# The dataclass function is the function that transform Beer into a dataclass
-# It implements all special methods and uses the Decorator Pattern
-@dataclass
-class Beer:
+
+# Inherit SQLModel class and set table as true to create a new database table
+class Beer(SQLModel, table=True):
+    # Defining an optional id as primary key (and without default) using Field
+    id: Optional[int] = Field(primary_key=True, default=None)
     name: str
     style: str
     flavor: int
     image: int 
     cost: int
 
+    # Method to validate ratings and raising exception
+    # @validator decorator is injecting code on function and telling all attributes to validate
+    @validator("flavor", "image", "cost")
+    def validate_ratings(cls, v, field):    
+        if v < 1 or v > 10:
+            # Raise is like throw new. TODO: what does f means?
+            raise RuntimeError(f"{field.name} must be between 1 and 10.")
+        return v    # Returning value as v
+
+
 # Creating an instance from model Beer
-first_beer = Beer(name="Brahma", style="Pilsen", flavor=8, image=2, cost=9)
+# Using python -i beerlog/models.py:
+# print(select(Beer)) returns the select SQL query
+# SELECT beer.id, beer.name, beer.style, beer.flavor, beer.image, beer.cost FROM beer
+# Which means that beer is a table on database.
+# Other query example: select(Beer).where(Beer.name == "Brahma")
+# It uses SQLAlchemy and Pydentic behind the scenes
+# (https://www.sqlalchemy.org/ and https://pydantic-docs.helpmanual.io/). 
+# So SQLModel is an abstraction of SQLAlchemy and Pydentic, table from SQLAlchemy and a Pydent Model
 
-# using python -i models.py:
-
-# dir(first_beer), we'll have the list 
-# ['__annotations__', '__class__', '__dataclass_fields__', '__dataclass_params__', '__delattr__', 
-# '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__',
-#  '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__',
-#  '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', 
-# '__weakref__', 'cost', 'flavor', 'image', 'name', 'style']
-# The dunder attributes represents the dataclass attributes
-
-# By object.attribute_name we can have the value (ex.: first_beer.name)
+# We can do a try catch with the validation error
+try:
+    first_beer = Beer(name="Brahma", style="Pilsen", flavor=8, image=2, cost=9)
+except RuntimeError:
+    print("Validation Error!")
